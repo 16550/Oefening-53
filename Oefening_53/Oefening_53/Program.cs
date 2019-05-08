9ab1178b8bc835e5ae4d1c47044df7d703696cca
16550jve <jaspervenderickx@hotmail.com>
2019-05-08 20:29:45 +0200

    update

--- Oefening_53/Oefening_53/Program.cs
+++ Oefening_53/Oefening_53/Program.cs
@@ -6,7 +6,7 @@ namespace Oefening_53
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Class!");
         }
     }
 }
